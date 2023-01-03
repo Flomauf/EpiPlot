2bf187640cec2df81ff668f0cde3f0e5825802cf
Florian Mauffrey <32743829+Flomauf@users.noreply.github.com>
2023-01-03 12:10:52 +0100

    Visual updates

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 # Install packages if not present
 list.of.packages <- c("ggplot2", "shiny", "dplyr", "forcats", "shinyWidgets",
                       "svglite", "shinydashboard", "plotly", "shinycssloaders",
-                      "lubridate", "GGally", "network", "sna")
+                      "lubridate", "GGally")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -16,7 +16,7 @@ library(shinydashboard)
 library(plotly)
 library(shinycssloaders)
 library(lubridate)
-library(GGally)
+library(ggnet)
 library(network)
 library(sna)
 library(scales)
